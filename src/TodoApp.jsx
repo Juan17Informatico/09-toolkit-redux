a7bc5d7929b9564bb39063d95d555e0b9fab59fc
JuanCM <juancampuzano2356@gmail.com>
2024-05-15 16:02:07 -0500

    Final sección 18

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,15 +1,43 @@
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/api"
 
 export const TodoApp = () => {
+
+  const [todoId, setTodoId] = useState(1)
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1); 
+  }
+
+  const prevTodo = () => {
+    if( todoId === 1) return;
+    setTodoId( todoId - 1 ); 
+  }
+
   return (
     <>
         <h1>Todos - RTK Query</h1>
         <hr />
-        <h4>isLoading...</h4>
+        <h4>isLoading { isLoading ? 'True' : 'False' }</h4>
 
-        <pre>...</pre>
+        <pre>{ JSON.stringify(todo) }</pre>
+        <button onClick={prevTodo}>Prev Todo</button>
+        <button onClick={nextTodo}>Next Todo</button>
 
+{/* 
+        <ul>
+          { todos.map(todo => (
+            <li key={todo.id}>
+              <strong style={{fontSize: '18px'}}>
+                { todo.completed ? 'Done' : 'Pending'}
+              </strong>
+              { todo.title }
+            </li>
+          ) ) }
+        </ul> */}
 
-        <button>Next Todo</button>
     </>
   )
 }
